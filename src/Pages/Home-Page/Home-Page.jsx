85814499cd16003a8058de5d17d6e2af5fa89d96
Abdallah Khemnou <abdallahkhemnou@gmail.com>
2026-06-19 20:43:59 +0100

    build the footre for all pages

--- src/Pages/Home-Page/Home-Page.jsx
+++ src/Pages/Home-Page/Home-Page.jsx
@@ -1,11 +1,27 @@
 import './Home-Page.css'
 import Header from "../../shared/Header/Header";
-// import Footer from "../../shared/Footer/Footer";
+import Footer from "../../shared/Footer/Footer";
 
 function Home() { 
   return (
     <>
       <Header />
+      <main>
+        <section className="hero">
+          <div></div>
+          <div></div>
+          <button></button>
+        </section>
+        <section className="picture"></section>
+        <section className="adds">
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <section className='motivation'></section>
+        <section className='end'></section>
+      </main>
+      <Footer />
     </>
   )
 }
--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,10 +1,19 @@
-import './footer.css'
+import "./Footer.css";
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { SiBluesky } from 'react-icons/si'
+import { SiTiktok } from 'react-icons/si'
 function Footer () {
   return (
     <>
       <div className='footer'>
-        
+        <p>Made with ❤️ and 🥗</p>
+        <div className='right'>
+          <AiOutlineInstagram />
+          <SiBluesky />
+          <SiTiktok />
+        </div>
       </div>
     </>
   )
 }
+export default Footer
